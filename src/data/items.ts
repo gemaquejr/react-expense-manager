6cae6b57a7606bd05058370446cfb3d948d8d361
gemaquejr <gemaquejr@hotmail.com>
2024-04-01 20:35:24 -0300

    Adiciona dados
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2024, 1, 1), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2024, 1, 1), category: 'food', title: 'Habibs', value: 12.15 },
+    { date: new Date(2024, 1, 1), category: 'rent', title: 'Aluguel', value: 2200 },
+    { date: new Date(2024, 1, 1), category: 'salary', title: 'Salário ACME', value: 6000 },
+];
