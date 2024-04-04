26f52a10cfe7af2e1ab606d0554f5b10a671d76e
gemaquejr <gemaquejr@hotmail.com>
2024-04-04 19:11:37 -0300

    Adiciona head no componente TableArea
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 
+import { TableArea } from './components/TableArea';
+
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
@@ -23,7 +25,7 @@ const App = () => {
         </C.HeaderText>
       </C.Header>
       <C.Body>
-        
+        <TableArea />        
       </C.Body>
     </C.Container>
   );
---
+++ src/components/TableArea/index.tsx
@@ -0,0 +1,16 @@
+import * as C from './styles'
+
+export const TableArea = () => {
+    return (
+        <C.Table>
+            <thead>
+                <tr>
+                    <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>
+                    <C.TableHeadColumn width={130}>Categoria</C.TableHeadColumn>
+                    <C.TableHeadColumn>Titulo</C.TableHeadColumn>
+                    <C.TableHeadColumn width={150}>Valor</C.TableHeadColumn>
+                </tr>
+            </thead>
+        </C.Table>
+    )
+}
