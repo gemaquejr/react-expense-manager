ece734c993598422124efb1b8b26ecdf306a1784
gemaquejr <gemaquejr@hotmail.com>
2024-04-04 18:26:21 -0300

    Armazena informações para usar depois (useState)
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  
   return (
     <C.Container>
       <C.Header>
