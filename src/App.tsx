d5f5cf55ba73dc40b8d4ee9290cb0845664047f0
gemaquejr <gemaquejr@hotmail.com>
2024-04-04 18:34:25 -0300

    Adiciona estado para buscar o mês atual
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Item } from './types/Item';
 import { Category } from './types/Category';
 import { categories } from './data/categories';
 import { items } from './data/items';
+import { getCurrentMonth } from './helpers/dateFilter';
 
 const App = () => {
   const [list, setList] = useState(items);
-  
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
   return (
     <C.Container>
       <C.Header>
