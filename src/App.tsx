f8ebb9b7adb1af24a979ebe1571451827e837907
gemaquejr <gemaquejr@hotmail.com>
2024-04-04 19:50:05 -0300

    Adiciona lista filtrada no componente TableArea
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
         </C.HeaderText>
       </C.Header>
       <C.Body>
-        <TableArea />        
+        <TableArea list={filteredList} />        
       </C.Body>
     </C.Container>
   );
