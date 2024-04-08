cf42efaea51126f0b4865aef4c5df96920765154
gemaquejr <gemaquejr@hotmail.com>
2024-04-08 20:05:23 -0300

    Muda dinâmicamente nome da categoria
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -11,7 +11,11 @@ export const TableItem = ({ item }: Props) => {
     return (
         <C.TableLine> 
             <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
-            <C.TableColumn>{categories[item.category].title}</C.TableColumn> 
+            <C.TableColumn>
+                <C.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </C.Category>
+            </C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>          
             <C.TableColumn>R$ {item.value}</C.TableColumn>                         
         </C.TableLine>
