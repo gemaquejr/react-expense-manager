c299872aba4fccd107506f3280ce4e9b3e70e68e
gemaquejr <gemaquejr@hotmail.com>
2024-04-09 19:14:47 -0300

    Adiciona função que muda dinâmicamente o mês em InfoArea
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -30,3 +30,9 @@ export const formatDate = (date: Date): string => {
 }
 
 const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`;
+
+export const formatCurrentMonth = (currentMonth: string): string => {
+    let [year, month] = currentMonth.split('-');
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    return `${months[parseInt(month) - 1]} de ${year}`;  // -1 porque o mês é 1-12, não 0-11. 
+}
