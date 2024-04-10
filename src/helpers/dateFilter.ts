661a5f448b33a9cd9dd56c9462200e9d0d2a119a
gemaquejr <gemaquejr@hotmail.com>
2024-04-10 18:09:39 -0300

    Muda dinâmicamente uma despesa ou um lucro em ResumeItem
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -36,3 +36,8 @@ export const formatCurrentMonth = (currentMonth: string): string => {
     let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
     return `${months[parseInt(month) - 1]} de ${year}`;  // -1 porque o mês é 1-12, não 0-11. 
 }
+
+export const newDateAdjusted = (dateField: string) => {
+    let [year, month, day] = dateField.split('-')
+    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
+  }
